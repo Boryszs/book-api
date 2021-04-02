b62c8f1c34241e1d6d44f4120e5095848d71a555
Boryszs <12damie@gmail.com>
2021-04-02 19:43:54 +0200

    Add Readme change style on login and register page.

---
+++ READEME.md
@@ -0,0 +1,31 @@
+# Book-API
+
+#### Simple client-server project
+
+## Technologies:
+- Java 11
+- Spring Boot
+- Spring Data JPA
+- H2 Database
+- JsonWebToken
+- JavaScript Cookie
+- JQuery/Javascript
+
+
+## Database H2
+``` sh
+http://127.0.0.1:8080/h2-console/
+Password:root
+```
+
+## Swgger Documentation
+``` sh
+http://127.0.0.1:8080/swagger-ui.html
+```
+
+![Zrzut ekranu 2021-04-02 193542](https://user-images.githubusercontent.com/62262900/113439724-67065d00-93eb-11eb-83c9-133f5065892a.png)
+
+![Zrzut ekranu 2021-04-02 193751](https://user-images.githubusercontent.com/62262900/113439759-72598880-93eb-11eb-9f4b-d4d57c2655d8.png)
+
+
+![book](https://user-images.githubusercontent.com/62262900/113439776-80a7a480-93eb-11eb-85e1-119d1a398152.gif)
--- src/main/java/com/example/book/config/SwaggerConfig.java
+++ src/main/java/com/example/book/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.example.book.config;
 import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContext;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,12 +26,17 @@ public class SwaggerConfig {
     }
 
     private Predicate<String> postPaths() {
-        return or(regex("/book.*"), regex("/author.*"),regex("/test.*"),regex("/user.*"),regex("/book.*"),regex("/reservation.*"));
+        return or(regex("/book.*"), regex("/author.*"), regex("/test.*"), regex("/user.*"), regex("/book.*"), regex("/reservation.*"));
     }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Library API")
                 .description("Library API")
                 .version("1.0").build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("jwtToken", "Authorization", "header");
     }
 }
